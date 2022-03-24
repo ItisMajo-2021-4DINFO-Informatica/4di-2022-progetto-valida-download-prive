cee8c10ebf85e98911c1b8426827c6e0f38a5052
Gabriele Cancemi <87067748+gabrielecancemi@users.noreply.github.com>
2022-03-24 13:03:09 +0100

    Pagine app
    Abbiamo scritto il codice che serve per navigare tra le "pagine" della nostra applicazione

--- 03-codice/PrivèValidaDownloadApp/PrivèValidaDownloadApp/MainWindow.xaml.cs
+++ 03-codice/PrivèValidaDownloadApp/PrivèValidaDownloadApp/MainWindow.xaml.cs
@@ -22,21 +22,110 @@ namespace PrivèValidaDownloadApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        String[] uno = { "Seleziona il tuo file (.iso)", "Nessun file selezionato" };
+        String[] due = { "Seleziona il tuo file (.sha256)", "Nessun file selezionato" };
+        String[] tre = { "Seleziona il tuo file (.asc)", "Nessun file selezionato" };
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void BtnImporta_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "(*.iso)|*.iso";
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            if (openFileDialog.ShowDialog() == true)
+            if(BtnImporta.Content == "Ripristina")
             {
-                foreach (string filename in openFileDialog.FileNames)
-                lblFile.Content = System.IO.Path.GetFileName(filename);
+                PaginaPrima();
+                BtnAvanti.IsEnabled = true;
+                BtnAvanti.Content = "Avanti";
+                BtnImporta.Content = "Seleziona";
+            }
+            else
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = true;
+                openFileDialog.Filter = "(*.iso)|*.iso";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    foreach (string filename in openFileDialog.FileNames)
+                        lblFile.Content = System.IO.Path.GetFileName(filename);
+                }
+            }
+            
+            
+        }
+
+        private void BtnAvanti_Click(object sender, RoutedEventArgs e)
+        {
+            if(BtnAvanti.Content == "Esegui")
+            {
+                
+                
+                
+                lblIndice.Content = "Fine";
+                BtnAvanti.IsEnabled = false;
+                BtnIndietro.IsEnabled = false;
+                LblImporta.Content = "Inserire gli errori trovati";
+                BtnImporta.Content = "Ripristina";
+                lblFile.Content = "";
+                pbProgress.Value = 100;
+            }
+
+
+
+
+            string pagina = (string)lblIndice.Content;
+           if(pagina == "1.")
+            {
+                lblIndice.Content = "2.";
+                LblImporta.Content = due[0];
+                lblFile.Content = due[1];
+                pbProgress.Value = 33;
+                BtnIndietro.IsEnabled = true;
+           }
+           if(pagina == "2.")
+            {
+                lblIndice.Content = "3.";
+                LblImporta.Content = tre[0];
+                lblFile.Content = tre[1];
+                pbProgress.Value = 66;
+                BtnAvanti.Content = "Esegui";
+            }
+           
+
+
+        }
+
+
+        public void PaginaPrima()
+        {
+            lblIndice.Content = "1.";
+            LblImporta.Content = uno[0];
+            lblFile.Content = uno[1];
+            pbProgress.Value = 0;
+            BtnIndietro.IsEnabled = false;
+        }
+
+
+        private void BtnIndietro_Click(object sender, RoutedEventArgs e)
+        {
+            string pagina = (string)lblIndice.Content;
+
+            if (pagina == "2.")
+            {
+                PaginaPrima();
+            }
+            if (pagina == "3.")
+            {
+                lblIndice.Content = "2.";
+                LblImporta.Content = due[0];
+                lblFile.Content = due[1];
+                pbProgress.Value = 33;
+                BtnAvanti.Content = "Avanti";
             }
         }
     }
