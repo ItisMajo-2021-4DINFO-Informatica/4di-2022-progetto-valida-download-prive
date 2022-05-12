38c03d2a00000daae9e9bfb1fcac1b8847635ce2
Soudqi Nizar <soudqinizar@gmail.com>
2022-05-12 12:41:13 +0200

    Test finali PGP
    Stiamo eseguendo dei test finali per poi procedere con la consegna definita.

--- 03-codice/PrivèValidaDownloadApp/PrivèValidaDownloadApp/GPG.cs
+++ 03-codice/PrivèValidaDownloadApp/PrivèValidaDownloadApp/GPG.cs
@@ -28,7 +28,7 @@ namespace PrivèValidaDownloadApp
                     pProcess.StartInfo.FileName = "cmd.exe";
                     pProcess.StartInfo.Arguments = "/k gpg --recv-keys " + finger ;
                     pProcess.Start();
-                    pProcess.WaitForExit(1000);
+                    pProcess.WaitForExit(3000);
                 }
 
                 using (Process pProcess = new Process())
